4ce6fe71f8a7345f6c9c418e42f5dbd45f3695f2
JensB <Jens.j.Bonten@gmail.com>
2026-03-23 17:57:44 +0100

    Converted data and changed names for the collumns

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel("data\Dagens lengde (2).xlsx")
 
+
 print(f"antall kolonner: {len(df.columns)}")
 print(f"antall rader: {len(df)}")
 print()
@@ -13,9 +14,18 @@ for kolonne in df.columns:
     
 print("\nFørste 5 rader: ")
 print(df.head())
+ 
 
-
-for i in range (len(df.head())):
+for i in range(5):
     print (f"Rad {i}:")
     print(df.iloc[i])
     print()
+    
+    
+df.columns = ["Date", "day_length", "sunrise", "sunset", "daily_increase", "total_increase"]
+df["date"] = pd.to_datetime(df["Date"], format="%d.%m.%y")
+df["day_length"] = pd.to_timedelta(df("day_length"))
+df["daily_increase"] = pd.to_timedelta(df("daily_increase"))
+df["total_increase"] = pd.to_timedelta(df("total_increase"))
+df["sunrise"] = pd.to_datetime(df["sunrise"], format="%H:%M:%S")
+df["sunset"] = pd.to_datetime(df["sunset"], format="%H:%M:%S")
